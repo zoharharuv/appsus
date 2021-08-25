0ab8aeb4890e209921fb18cf90bcf9c45656f826
Zohar H <misterblat@gmail.com>
2021-08-25 23:49:38 +0300

    more css

--- js/apps-js/mail/cmps/MailPreview.jsx
+++ js/apps-js/mail/cmps/MailPreview.jsx
@@ -6,7 +6,6 @@ export class MailPreview extends React.Component {
                 <button onClick={() => onStarMail(mail)}>
                     <img src={`./img/${mail.isStarred ? 'star-full' : 'star-empty'}.svg`} />
                 </button>
-                <button onClick={() => onDeleteMail(mail)}><img src="./img/delete.svg" /></button>
                 <div className="mail-preview-content" onClick={() => onSelectMail(mail)}>
                     <h2>{mail.to.split('@')[0]} </h2>
                     <h2>{mail.subject.substring(0, 50)}</h2>
@@ -14,6 +13,7 @@ export class MailPreview extends React.Component {
                 <button onClick={() => onToggleRead(mail)}>
                     <img src={`./img/${mail.isRead ? 'read' : 'unread'}.svg`} />
                 </button>
+                <button onClick={() => onDeleteMail(mail)}><img src="./img/delete.svg" /></button>
             </article>
         );
     }
