03094e65cfb38e275bee9932fde95100e7392785
Zohar H <misterblat@gmail.com>
2021-08-26 14:34:30 +0300

    fix css v1

--- js/Home.jsx
+++ js/Home.jsx
@@ -21,7 +21,7 @@ export function Home() {
   return (
     <section className="home">
       <img src="img/hero.png" alt="appsus" className="hero" />
-      <h1>Welcome to Appsus</h1>
+      <h1>Welcome to Appsus </h1>
       <h3>Make your life easier and much more productive with Appsus: Books, Mails and Notes services!</h3>
       <p>Made with love by Zohar and Guy</p>
 
